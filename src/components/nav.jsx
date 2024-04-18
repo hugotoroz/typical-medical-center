f1dfa7b80d11547e986b0e4ea15d19fff35ff74d
Vicente :^l <101284198+DryusFar@users.noreply.github.com>
2024-04-18 00:41:03 -0400

    navbar simple 18-04

---
+++ src/components/nav.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FaBriefcaseMedical } from "react-icons/fa";
+import "../css/navbar.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+  
+
+const PrincipalNavbar = () => {
+
+    return (
+        <>
+        <Navbar collapseOnSelect expand="lg" className="navbar" fixed="top" >
+        <Container fluid>
+            <Navbar.Brand href="#home"><FaBriefcaseMedical  style={{ fontSize: '1.9em' }}/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">About us</Nav.Link>
+                <Nav.Link href="#record">Record</Nav.Link>
+                <Nav.Link href="#doctors">Doctors</Nav.Link>
+                <NavDropdown title="Services" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Juan</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                    Gana
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Goty</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                    Toty
+                </NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            <Nav>
+                <Nav.Link href="#deets"  style={{ fontWeight: 'bold', fontSize: '1.2em' }}>Typical Medical Center</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+        </>
+    );
+  }
+
+export default PrincipalNavbar;
