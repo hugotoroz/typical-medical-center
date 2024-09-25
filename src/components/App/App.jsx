91784e5bb052d48fb34392f5bb99820507e75d62
Kevin Oyaneder Velásquez <101284321+Kviinalx@users.noreply.github.com>
2024-09-25 18:49:38 -0300

    Login

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import Index from '../../pages/index/index';
 import Login from '../../pages/login/login';
 import Contact from '../../pages/contact/contact';
 import './App.css'
+import Register from '../../pages/register/register';
 
 ////Configures routes and renders the application with routing.
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </>
