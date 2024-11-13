2a3f4881597c8f47833fe0a77c49d3da93be4638
Vicente :^l <101284198+DryusFar@users.noreply.github.com>
2024-11-12 21:34:24 -0300

    Pdf implementation

--- src/pages/admin/adminManagment/adminManagment.jsx
+++ src/pages/admin/adminManagment/adminManagment.jsx
@@ -19,6 +19,7 @@ const AdminManagment = () => {
   const [openId, setOpenId] = useState(null); // State to store user ID open
   const [data, setData] = useState([]); // Estado para almacenar los datos obtenidos de la API
   const [error, setError] = useState(null); // Estado para manejar errores
+  const [isLoading, setIsLoading] = useState(true); // Estado para el spinner de carga
 
   const token = sessionStorage.getItem('token');
 
@@ -28,6 +29,7 @@ const AdminManagment = () => {
 
   // Función para obtener los datos de la API usando Axios
   const fetchData = async () => {
+    setIsLoading(true);
     try {
       const response = await axios.get(`${API_URL}/doctors`, {
         headers: {
@@ -44,9 +46,11 @@ const AdminManagment = () => {
       }));
 
       setData(filteredData); // Actualizar el estado con los datos filtrados
+      setIsLoading(false); // Finaliza la carga
     } catch (error) {
       setError('Error fetching data'); // Manejar error si la API falla
       console.error('Error fetching data:', error);
+      setIsLoading(false); // Finaliza la carga
     }
   };
 
@@ -58,55 +62,62 @@ const AdminManagment = () => {
           <div className="container mx-auto flex-grow px-4">
           <h2 className="text-2xl font-semibold mb-6">Doctores</h2>
           {error && <p className="text-red-500">{error}</p>} {/* Mostrar error si existe */}
-            <div className="overflow-x-auto">
-              <table className="min-w-full max-w-xs bg-white border border-gray-300 rounded-lg shadow-lg">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th className="px-4 py-2 border-b border-gray-200 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
-                    <th className="px-4 py-2 border-b border-gray-200 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Rut</th>
-                    <th className="px-4 py-2 border-b border-gray-200 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                    <th className="px-4 py-2 border-b border-gray-200 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-                    <th className="px-4 py-2 border-b border-gray-200 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map((user) => (
-                    <tr key={user.id} className="hover:bg-gray-100 transition-colors duration-200">
-                      <td className="px-4 py-3 border-b border-gray-200 text-sm">{user.id}</td>
-                      <td className="px-4 py-3 border-b border-gray-200 text-sm text-left">{user.rut}</td>
-                      <td className="px-4 py-3 border-b border-gray-200 text-sm text-left">{user.name}</td>
-                      <td className="px-4 py-3 border-b border-gray-200 text-sm text-left">{user.email}</td>
-                      <td className="px-4 py-3 border-b border-gray-200 text-sm">
-                        <div className="flex items-center justify-center">
-                          <motion.div animate={openId === user.id ? "open" : "closed"} className="absolute">
-                            <button
-                              onClick={() => setOpenId(openId === user.id ? null : user.id)} // Alternate Id
-                              className="flex items-center gap-2 px-3 py-1.5 rounded-md text-indigo-50 bg-indigo-500 hover:bg-indigo-500 transition-colors z-10"
-                            >
-                              <span className="font-medium text-sm">Post actions</span>
-                              <motion.span variants={iconVariants}>
-                                <FiChevronDown />
-                              </motion.span>
-                            </button>
-
-                            <motion.ul
-                              initial={wrapperVariants.closed}
-                              variants={wrapperVariants}
-                              style={{ originY: "top", translateX: "-50%" }}
-                              className="flex flex-col gap-2 p-2 rounded-lg bg-white shadow-xl absolute top-[120%] left-[50%] w-48 overflow-hidden z-20"
-                            >
-                              <Option setOpen={setOpenId} Icon={FiEdit} text="Modificar" />
-                              <Option setOpen={setOpenId} Icon={FiEyeOff} text="Desactivar" rut={user.rut}/>
-                              <Option setOpen={setOpenId} Icon={FiTrash} text="Eliminar" rut={user.rut}/>
-                            </motion.ul>
-                          </motion.div>
-                        </div>
-                      </td>
+            {isLoading ? (
+              // Muestra solo el spinner mientras se cargan los datos
+              <div className="flex justify-center items-center h-64">
+                <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+              </div>
+            ) : (
+              <div className="overflow-x-auto">
+                <table className="min-w-full max-w-xs bg-white border border-gray-300 rounded-lg shadow-lg">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th className="px-4 py-2 border-b border-gray-200 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+                      <th className="px-4 py-2 border-b border-gray-200 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Rut</th>
+                      <th className="px-4 py-2 border-b border-gray-200 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                      <th className="px-4 py-2 border-b border-gray-200 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                      <th className="px-4 py-2 border-b border-gray-200 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                  </thead>
+                  <tbody>
+                    {data.map((user) => (
+                      <tr key={user.id} className="hover:bg-gray-100 transition-colors duration-200">
+                        <td className="px-4 py-3 border-b border-gray-200 text-sm">{user.id}</td>
+                        <td className="px-4 py-3 border-b border-gray-200 text-sm text-left">{user.rut}</td>
+                        <td className="px-4 py-3 border-b border-gray-200 text-sm text-left">{user.name}</td>
+                        <td className="px-4 py-3 border-b border-gray-200 text-sm text-left">{user.email}</td>
+                        <td className="px-4 py-3 border-b border-gray-200 text-sm">
+                          <div className="flex items-center justify-center">
+                            <motion.div animate={openId === user.id ? "open" : "closed"} className="absolute">
+                              <button
+                                onClick={() => setOpenId(openId === user.id ? null : user.id)} // Alternate Id
+                                className="flex items-center gap-2 px-3 py-1.5 rounded-md text-indigo-50 bg-indigo-500 hover:bg-indigo-500 transition-colors z-10"
+                              >
+                                <span className="font-medium text-sm">Post actions</span>
+                                <motion.span variants={iconVariants}>
+                                  <FiChevronDown />
+                                </motion.span>
+                              </button>
+
+                              <motion.ul
+                                initial={wrapperVariants.closed}
+                                variants={wrapperVariants}
+                                style={{ originY: "top", translateX: "-50%" }}
+                                className="flex flex-col gap-2 p-2 rounded-lg bg-white shadow-xl absolute top-[120%] left-[50%] w-48 overflow-hidden z-20"
+                              >
+                                <Option setOpen={setOpenId} Icon={FiEdit} text="Modificar" />
+                                <Option setOpen={setOpenId} Icon={FiEyeOff} text="Desactivar" rut={user.rut}/>
+                                <Option setOpen={setOpenId} Icon={FiTrash} text="Eliminar" rut={user.rut}/>
+                              </motion.ul>
+                            </motion.div>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
           </div>
         </div>
    
